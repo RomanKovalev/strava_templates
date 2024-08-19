4b53557b4d6cf9936982f92d2904d9edee388fb3
Roman Kovalev <rvk.sft@gmail.com>
2024-08-19 14:52:40 +0300

    added checkauth view

--- web/src/components/Logout.jsx
+++ web/src/components/Logout.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-// import { logout } from '../auth.js';
 import { logout as logoutAction } from '../store/authSlice';
 import {useNavigate} from "react-router-dom";
 import api from "../api.js";
 
 function Logout() {
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleLogout = async () => {
