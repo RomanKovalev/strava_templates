fecb6a5b46e330000f16efbac1ff1cfa9736c3f8
Roman Kovalev <rvk.sft@gmail.com>
2024-09-11 13:54:17 +0300

    should to be reverted

--- backend/activities/middlewares.py
+++ backend/activities/middlewares.py
@@ -1,27 +1,45 @@
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
-from datetime import datetime
+from datetime import datetime, timezone
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from rest_framework_simplejwt.exceptions import TokenError
 import jwt
-from django.conf import settings
+from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
+
 
 class JWTAuthCookieMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.path == '/api/strava/login/' or request.path == '/api/strava/callback/':
-            response = self.get_response(request)
-            return response
+        if request.path in ['/api/strava/login/', '/api/strava/callback/']:
+            return self.get_response(request)
 
         jwt_access_token = request.COOKIES.get('jwt_access')
         jwt_refresh_token = request.COOKIES.get('jwt_refresh')
 
-        if jwt_access_token is None and jwt_refresh_token:
+        try:
+            if jwt_access_token is not None:
+                decoded_token = jwt.decode(jwt_access_token, options={"verify_signature": False})
+                exp_timestamp = decoded_token.get('exp')
+
+                if exp_timestamp:
+                    # access_token = AccessToken(jwt_access_token)
+                    # exp_timestamp = access_token['exp']
+                    exp_time = datetime.utcfromtimestamp(exp_timestamp).replace(tzinfo=timezone.utc)
+
+                    current_time = datetime.now(timezone.utc)
+
+                    if exp_time < current_time:
+                        jwt_access_token = None
+        except (ExpiredSignatureError, InvalidTokenError):
+            jwt_access_token = None
+
+        if jwt_access_token is None and jwt_refresh_token is not None:
             try:
                 refresh_token = RefreshToken(jwt_refresh_token)
                 new_jwt_access_token = refresh_token.access_token
 
                 response = self.get_response(request)
+
                 response.set_cookie(
                     key='jwt_access',
                     value=str(new_jwt_access_token),
@@ -30,18 +48,17 @@ class JWTAuthCookieMiddleware:
                     secure=True,  # Включите в production
                     samesite='Lax'
                 )
-                request.META['HTTP_AUTHORIZATION'] = f'Bearer {jwt_access_token}'
+
+                request.META['HTTP_AUTHORIZATION'] = f'Bearer {new_jwt_access_token}'
                 return response
 
             except TokenError:
                 response = self.get_response(request)
                 response.delete_cookie('jwt_access')
                 response.delete_cookie('jwt_refresh')
-
-                response = self.get_response(request)
                 return response
-        if jwt_access_token and jwt_refresh_token:
+
+        if jwt_access_token is not None:
             request.META['HTTP_AUTHORIZATION'] = f'Bearer {jwt_access_token}'
 
-        response = self.get_response(request)
-        return response
+        return self.get_response(request)
